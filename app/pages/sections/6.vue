16e187ace685f02bcc7c8a91ebf3575c577caad3
toshisas <48203235+toshiakisan1127@users.noreply.github.com>
2026-09-03 18:14:37 +0900

    Slow down Russian text-to-speech playback

--- app/pages/sections/6.vue
+++ app/pages/sections/6.vue
@@ -87,7 +87,7 @@ const speak = (text: string) => {
 
   const utterance = new SpeechSynthesisUtterance(text)
   utterance.lang = 'ru-RU'
-  utterance.rate = 0.7
+  utterance.rate = 0.4
 
   const russianVoice = window.speechSynthesis
     .getVoices()
