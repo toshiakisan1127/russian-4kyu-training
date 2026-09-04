50fc5c7bdfa90c88822639827666f969ed3b3896
toshisas <48203235+toshiakisan1127@users.noreply.github.com>
2026-09-05 03:39:55 +0900

    revert: remove Exam 2 from mock registry

--- app/data/mockExams.ts
+++ app/data/mockExams.ts
@@ -1,9 +1,7 @@
 import { mockExam1 } from './mockExam1'
-import { mockExam2 } from './mockExam2'
 
 export type MockExam = typeof mockExam1
 
 export const mockExams: MockExam[] = [
   mockExam1,
-  mockExam2,
 ]
