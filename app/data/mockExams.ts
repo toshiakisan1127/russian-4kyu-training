0ba6ccb7761ed3103e55854c183c686ac4b92bbb
toshisas <48203235+toshiakisan1127@users.noreply.github.com>
2026-09-05 03:39:37 +0900

    Revert PR #123
    模試第3回と一覧登録を取り消す。CI #91成功確認済み。

--- app/data/mockExams.ts
+++ app/data/mockExams.ts
@@ -1,11 +1,9 @@
 import { mockExam1 } from './mockExam1'
 import { mockExam2 } from './mockExam2'
-import { mockExam3 } from './mockExam3'
 
 export type MockExam = typeof mockExam1
 
 export const mockExams: MockExam[] = [
   mockExam1,
   mockExam2,
-  mockExam3,
 ]
