8728749efffe4dda2d4ac230baca1711958eafe4
toshisas <48203235+toshiakisan1127@users.noreply.github.com>
2026-09-05 03:46:17 +0900

    feat: register mock exam 2

--- app/data/mockExams.ts
+++ app/data/mockExams.ts
@@ -1,7 +1,9 @@
 import { mockExam1 } from './mockExam1'
+import { mockExam2 } from './mockExam2'
 
 export type MockExam = typeof mockExam1
 
 export const mockExams: MockExam[] = [
   mockExam1,
+  mockExam2,
 ]
