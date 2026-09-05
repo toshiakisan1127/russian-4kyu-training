98943ab7c43271af380895fb6af3ff1a681b829a
toshisas <48203235+toshiakisan1127@users.noreply.github.com>
2026-09-05 16:01:12 +0900

    feat: show automatic last updated timestamp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      lastUpdatedAt: '',
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
