4a43eb9d1b6ef8506ced1a772b45a557516269d1
toshisas <48203235+toshiakisan1127@users.noreply.github.com>
2026-09-08 14:30:30 +0900

    feat: use soft pastel OGP image

--- server/routes/og-image.png.ts
+++ server/routes/og-image.png.ts
@@ -1,7 +1,14 @@
-import { readFileSync } from 'node:fs'
-import { fileURLToPath } from 'node:url'
+import part01 from '../utils/og-image/part01'
+import part02 from '../utils/og-image/part02'
+import part03 from '../utils/og-image/part03'
+import part04 from '../utils/og-image/part04'
+import part05 from '../utils/og-image/part05'
+import part06 from '../utils/og-image/part06'
 
-const ogImage = readFileSync(fileURLToPath(new URL('../../../public/og-image.png', import.meta.url)))
+const ogImage = Buffer.from(
+  part01 + part02 + part03 + part04 + part05 + part06,
+  'base64',
+)
 
 export default defineEventHandler((event) => {
   setHeader(event, 'content-type', 'image/png')
